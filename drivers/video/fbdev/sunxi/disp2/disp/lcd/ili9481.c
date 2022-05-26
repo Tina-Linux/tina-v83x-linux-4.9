d987ad9e8f1ca093dffe1cdae2361a3b30f17051
YuzukiTsuru <gloomyghost@gloomyghost.com>
2022-05-26 21:23:22 +0800

    fine

--- drivers/video/fbdev/sunxi/disp2/disp/lcd/ili9481.c
+++ drivers/video/fbdev/sunxi/disp2/disp/lcd/ili9481.c
@@ -91,22 +91,47 @@ static void LCD_panel_exit(u32 sel);
 
 extern void tcon_reset(u32 sel);
 
-/**
- * @name       :lcd_esd_check
- * @brief      :check if panel is ok
- * @param[IN]  :sel:index of dsi
- * @param[OUT] :none
- * @return     :0 if ok, else not ok
- */
-static s32 lcd_esd_check(u32 sel) {
-  // pr_info("[lcd_esd_check] %d\n", sel);
+static void LCD_cfg_panel_info(struct panel_extend_para *info) {
+  u32 i = 0, j = 0;
+  u32 items;
+  u8 lcd_gamma_tbl[][2] = {
+      //{input value, corrected value}
+      {0, 0},     {15, 15},   {30, 30},   {45, 45},   {60, 60},   {75, 75},
+      {90, 90},   {105, 105}, {120, 120}, {135, 135}, {150, 150}, {165, 165},
+      {180, 180}, {195, 195}, {210, 210}, {225, 225}, {240, 240}, {255, 255},
+  };
 
-  tcon_reset(sel);
+  u32 lcd_cmap_tbl[2][3][4] = {
+      {
+          {LCD_CMAP_G0, LCD_CMAP_B1, LCD_CMAP_G2, LCD_CMAP_B3},
+          {LCD_CMAP_B0, LCD_CMAP_R1, LCD_CMAP_B2, LCD_CMAP_R3},
+          {LCD_CMAP_R0, LCD_CMAP_G1, LCD_CMAP_R2, LCD_CMAP_G3},
+      },
+      {
+          {LCD_CMAP_B3, LCD_CMAP_G2, LCD_CMAP_B1, LCD_CMAP_G0},
+          {LCD_CMAP_R3, LCD_CMAP_B2, LCD_CMAP_R1, LCD_CMAP_B0},
+          {LCD_CMAP_G3, LCD_CMAP_R2, LCD_CMAP_G1, LCD_CMAP_R0},
+      },
+  };
 
-  return 0;
-}
+  items = sizeof(lcd_gamma_tbl) / 2;
+  for (i = 0; i < items - 1; i++) {
+    u32 num = lcd_gamma_tbl[i + 1][0] - lcd_gamma_tbl[i][0];
 
-static void LCD_cfg_panel_info(struct panel_extend_para *info) { return; };
+    for (j = 0; j < num; j++) {
+      u32 value = 0;
+
+      value = lcd_gamma_tbl[i][1] +
+              ((lcd_gamma_tbl[i + 1][1] - lcd_gamma_tbl[i][1]) * j) / num;
+      info->lcd_gamma_tbl[lcd_gamma_tbl[i][0] + j] =
+          (value << 16) + (value << 8) + value;
+    }
+  }
+  info->lcd_gamma_tbl[255] = (lcd_gamma_tbl[items - 1][1] << 16) +
+                             (lcd_gamma_tbl[items - 1][1] << 8) +
+                             lcd_gamma_tbl[items - 1][1];
+  memcpy(info->lcd_cmap_tbl, lcd_cmap_tbl, sizeof(lcd_cmap_tbl));
+};
 
 static s32 LCD_open_flow(u32 sel) {
   LCD_OPEN_FUNC(sel, LCD_power_on, 120);
